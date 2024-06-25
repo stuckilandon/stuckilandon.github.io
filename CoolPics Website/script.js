4c3a60d1bb2bb580762556f6ae8014dae0d5f9d6
stuckilandon <stuckilandon@gmail.com>
2024-06-25 16:26:41 -0600

    Fix

--- CoolPics Website/script.js
+++ CoolPics Website/script.js
@@ -10,4 +10,31 @@ menubutton.addEventListener('click', function() {
     } else {
         links.style.display = "none";
     }
+
 });
+
+document.addEventListener('DOMContentLoaded',function () {
+    const modal = document.querySelector('.viewer');
+    const modalImage = modal.querySelector('img');
+    const closeButton = document.querySelector('.close-viewer');
+
+
+    function openModal(event) {
+        modalImage.src = event.target.src; 
+        modalImage.alt = event.target.alt; 
+        modal.style.display = 'grid'; 
+    }
+
+
+    function closeModal() {
+        modal.style.display = 'none'; 
+    }
+
+
+    document.querySelectorAll('img[id^="picture"]').forEach(img => {
+        img.addEventListener('click', openModal);
+    });
+
+    closeButton.addEventListener('click', closeModal);
+});
+
